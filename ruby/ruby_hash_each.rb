2d26b2c11a4bd463bcfcd22846ba03a782757243
Leonardo Costa <leo.costa.programming@gmail.com>
2020-01-07 23:37:21 +0100

    ruby array each

---
+++ ruby/ruby_hash_each.rb
@@ -0,0 +1,11 @@
+def iter_hash(hash)
+  hash.each do |key, value|
+    puts key, value
+  end
+end
+
+# OR
+# def iter_hash(hash)
+#   # your code here
+#   hash.each { |key, value| puts key, value}
+# end
