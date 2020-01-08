5afe0f6138eebdd36184fc3faa7a96d295ab9dce
Leonardo Costa <leo.costa.programming@gmail.com>
2020-01-08 19:23:14 +0100

    ruby control structures - unless

---
+++ ruby/ruby_hash_addition_deletion_selection.rb
@@ -0,0 +1,9 @@
+# Enter your code here.
+hackerrank.store(543121, 100)
+hackerrank.keep_if {|key, value| key.is_a?(Integer)}
+hackerrank.delete_if {|key, value| key % 2 == 0}
+
+# Adding:
+# -A key-value pair [543121, 100] to the hackerrank object using store
+# -Retain all key-value pairs where keys are Integers ( clue : is_a? Integer )
+# -Delete all key-value pairs where keys are even-valued.
