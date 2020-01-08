87744a2b5f84f218be18e336f7e8414ecdf5a114
Leonardo Costa <leo.costa.programming@gmail.com>
2020-01-08 23:39:03 +0100

    ruby control structures - until

---
+++ ruby/ruby_control_structure_until.rb
@@ -0,0 +1,11 @@
+# Call the method coder.practice until coder.oh_one? becomes true.
+# Use the until control structure.
+
+until coder.oh_one?
+  do oder.practice
+end
+
+# OR
+
+# one-liner solution
+# until coder.oh_one? do coder.practice end
