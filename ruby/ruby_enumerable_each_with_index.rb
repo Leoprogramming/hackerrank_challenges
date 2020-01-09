603911fa456eab3b5938988e161bf8a2bfb3c5d0
Leonardo Costa <leo.costa.programming@gmail.com>
2020-01-09 22:43:55 +0100

    ruby control structures - each_with_index method alternative solutions

--- ruby/ruby_enumerable_each_with_index.rb
+++ ruby/ruby_enumerable_each_with_index.rb
@@ -6,3 +6,9 @@ def skip_animals(animals, skip)
   end
   results
 end
+
+# OR
+
+# def skip_animals(animals, skip)
+#   animals.map.with_index{|a, index| "#{index}:#{a}" if index >= skip}.compact
+# end
