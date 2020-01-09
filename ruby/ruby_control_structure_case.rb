131beccceace7bad40117bc8ba63c959a7ffc4b3
Leonardo Costa <leo.costa.programming@gmail.com>
2020-01-09 20:56:51 +0100

    fixing indentation

--- ruby/ruby_control_structure_case.rb
+++ ruby/ruby_control_structure_case.rb
@@ -1,15 +1,15 @@
 def identify_class(obj)
-    # write your case control structure here
+  # write your case control structure here
   case obj
-      when Hacker
-      puts "It's a Hacker!"
-      when Submission
-      puts "It's a Submission!"
-      when TestCase
-      puts "It's a TestCase!"
-      when Contest
-      puts "It's a Contest!"
+  when Hacker
+    puts "It's a Hacker!"
+  when Submission
+    puts "It's a Submission!"
+  when TestCase
+    puts "It's a TestCase!"
+  when Contest
+    puts "It's a Contest!"
   else
-      puts "It's an unknown model"
+    puts "It's an unknown model"
   end
 end
