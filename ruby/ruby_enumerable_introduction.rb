a80aa85f3079263d1bcc897e14f4ab7a8437a409
Leonardo Costa <leo.costa.programming@gmail.com>
2020-01-13 22:55:55 +0100

    added alternative solutions

--- ruby/ruby_enumerable_introduction.rb
+++ ruby/ruby_enumerable_introduction.rb
@@ -1,6 +1,24 @@
+# Challenge: Iterate over the items and return an Array containing the values.
+
 def iterate_colors(colors)
   # Your code here
   colors.map do |color|
     color
   end
 end
+
+# Better Solution ->
+
+# def iterate_colors(colors)
+#   # Your code here
+#   array = []
+#   colors.each { |n| array << n }
+#   return array
+# end
+
+# Creating an enumerable ->
+
+# def iterate_colors(colors)
+#   # Your code here
+#   colors.enum_for.to_a
+# end
