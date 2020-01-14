de5fd24802205c807067378b74dbfa0b2d43d004
Leonardo Costa <leo.costa.programming@gmail.com>
2020-01-14 16:05:36 +0100

    simple array sum solution

---
+++ ruby/simple_array_sum.rb
@@ -0,0 +1,21 @@
+#!/bin/ruby
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar)
+    ar.sum
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+ar_count = gets.to_i
+
+ar = gets.rstrip.split(' ').map(&:to_i)
+
+result = simpleArraySum ar
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
