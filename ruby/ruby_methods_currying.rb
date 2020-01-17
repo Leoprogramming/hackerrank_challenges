fe397cd7bb6bab3358a8c3b51679fcacf0723cd7
Leonardo Costa <leo.costa.programming@gmail.com>
2020-01-17 20:03:44 +0100

    ruby strings methods I

--- ruby/ruby_methods_currying.rb
+++ ruby/ruby_methods_currying.rb
@@ -21,4 +21,4 @@ base = gets.to_i
 raise_to_power = power_function.curry[base]
 
 power = gets.to_i
-puts raise_to_power.(power)
+puts raise_to_power.call(power)
---
+++ ruby/ruby_strings_methods_I.rb
@@ -0,0 +1,8 @@
+# In this challenge, your task is to code a process_text method, which takes an array
+# of strings as input and returns a single joined string
+# with all flanking whitespace and new lines removed.
+# Each string has to be separated by a single space.
+
+def process_text(array)
+  array.map { |c| c.chomp.strip }.join(' ')
+end
