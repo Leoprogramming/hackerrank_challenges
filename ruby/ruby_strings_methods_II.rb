a688c4754a73826cb9f129f6f4dbf05ebcac6c4a
Leonardo Costa <leo.costa.programming@gmail.com>
2020-01-21 22:40:17 +0100

    ruby strings methods II challenge

---
+++ ruby/ruby_strings_methods_II.rb
@@ -0,0 +1,18 @@
+# In this challenge, your task is to write the following methods:
+
+# mask_article which appends strike tags around certain words in a text.
+# The method takes 2 arguments: A string and an array of words.
+# It then replaces all the instances of words in the text with the modified version.
+# A helper method strike, given one string, appends strike off HTML tags around it.
+# The strike off HTML tag is <strike></strike>.
+
+# Apply the helper method in completing your main method.
+
+def mask_article(text, words)
+  words.each { |a| text.gsub!(a, strike(a)) }
+  text
+end
+
+def strike(s)
+  "<strike>#{s}</strike>"
+end
