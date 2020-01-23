c9ba50b6dab1df8b1316c3725b106db2e2b303bf
Leonardo Costa <leo.costa.programming@gmail.com>
2020-01-23 15:48:02 +0100

    ruby strings - introduction challenge

---
+++ ruby/ruby_strings_introduction.rb
@@ -0,0 +1,19 @@
+# In this introductory challenge, your task is to use
+# each of the above three methods to return the text Hello World and others!
+
+def single_quote
+  # single quote string here
+  'Hello World and others!'
+end
+
+def double_quote
+  # Double quote string here
+  "Hello World and others!"
+end
+
+def here_doc
+  # Here doc string here
+  <<-HEREDOC
+  Hello World and others!
+  HEREDOC
+end
