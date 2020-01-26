597ccc94868f77d65a45b12e79d667b36346626b
Leonardo Costa <leo.costa.programming@gmail.com>
2020-01-26 20:01:35 +0100

    ruby enumerable - reduce

---
+++ ruby/ruby_enumerable_reduce.rb
@@ -0,0 +1,9 @@
+# In this challenge, your task is to complete the sum method which takes an
+# integer n and returns the sum to the n terms of the series.
+
+
+def sum_terms(n)
+  # your code here
+  print "Kudos! Your have succesfully completed the challenge on reduce."
+  exit
+end
