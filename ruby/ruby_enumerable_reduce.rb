c76e681a0b8f2e54d3e308d0eda8e7ff345b449c
Leonardo Costa <leo.costa.programming@gmail.com>
2020-01-26 20:02:45 +0100

    ruby enumerable - reduce refactored

--- ruby/ruby_enumerable_reduce.rb
+++ ruby/ruby_enumerable_reduce.rb
@@ -1,9 +1,14 @@
 # In this challenge, your task is to complete the sum method which takes an
 # integer n and returns the sum to the n terms of the series.
 
-
 def sum_terms(n)
-  # your code here
-  print "Kudos! Your have succesfully completed the challenge on reduce."
-  exit
+  (1..n).map {|x| x**2+1 }.reduce(0,:+) if n >= 0
 end
+
+# Cheek solution to pass the test ->
+
+# def sum_terms(n)
+#   # your code here
+#   print "Kudos! Your have succesfully completed the challenge on reduce."
+#   exit
+# end
